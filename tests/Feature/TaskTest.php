4d47e01691fd1e6872004ca2c5d6d823afc3fddf
Kerem Kaya <kerem@mgssoftware.net>
2022-10-25 18:05:01 +0300

    test scripts updated

--- tests/Feature/TaskTest.php
+++ tests/Feature/TaskTest.php
@@ -5,6 +5,8 @@ namespace Tests\Feature;
 use App\Models\Task;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
+use Database\Factories\TaskFactory;
+use App\Http\Controllers\TaskController;
 
 class TaskTest extends TestCase
 {
@@ -23,10 +25,7 @@ class TaskTest extends TestCase
 
         $response->assertStatus(200);
     }
-//    public function test_database_is_empty(): void
-//    {
-//
-//    }
+
     public function test_creating_task(): void
     {
         $task = Task::create([
@@ -65,8 +64,53 @@ class TaskTest extends TestCase
         $task = Task::first();
         $this->assertDatabaseCount('tasks',1);
         $task->delete();
-        $task = Task::first();
         $this->assertDatabaseCount('tasks',0);
+
+    }
+    public function test_task_factory()
+    {
+        Task::factory(20)->create();
+        $this->assertDatabaseCount('tasks',20);
+    }
+
+    public function test_post_method_store()
+    {
+        $response = $this->post('/tasks',['description'=>'desc-1']);
+        $response->assertStatus(201);
+        $this->assertDatabaseCount('tasks',1);
+    }
+    public function test_store_method()
+    {
+        $response = $this->post('/tasks',['description'=>'desc-1']);
+        $response->assertStatus(201);
+        $this->assertDatabaseCount('tasks',1);
+    }
+
+    public function test_destroy_method()
+    {
+        $task = Task::create([
+            'description'=>'desc-1'
+        ]);
+        $this->assertDatabaseCount('tasks',1);
+        $this->assertDatabaseHas('tasks',['description'=>'desc-1']);
+
+        $response = $this->delete('/tasks/'.$task->id);
+
+        $this->assertDatabaseCount('tasks',0);
+        $response->assertStatus(204);
+
+    }
+    public function test_update_method()
+    {
+        $task = Task::create([
+            'description' => 'Des-1'
+        ]);
+        $this->assertDatabaseHas('tasks',['description'=>'Des-1']);
+        $this->assertDatabaseCount('tasks',1);
+
+        $this->put('/tasks/'.$task->id,['description'=>'New Des-1']);
+        $this->assertDatabaseHas('tasks',['description'=>'New Des-1']);
+        $this->assertDatabaseCount('tasks',1);
 
     }
 }
